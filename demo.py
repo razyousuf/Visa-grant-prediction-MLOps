bce41a6c9655c3aa7179309a3b5c63bb561167d8
razyousuf <23010492@hope.ac.uk>
2025-05-01 23:18:52 +0100

    Data Ingestion Completed

--- demo.py
+++ demo.py
@@ -1,11 +1,17 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-logging.info("This is to test the custom logging.")
+obj = TrainPipeline()
+obj.run_pipeline()
 
-try:
-    a = 2/0
-    print(a)
-except Exception as e:
-    raise USvisaException(e, sys)
+
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+
+# logging.info("This is to test the custom logging.")
+
+# try:
+#     a = 2/0
+#     print(a)
+# except Exception as e:
+#     raise USvisaException(e, sys)
---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,53 @@
+import os
+from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "visa_data"
+
+MONGODB_URL_KEY = "MONGODB_CREDENTIAL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
+MODEL_FILE_NAME = "model.pkl"
+
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
