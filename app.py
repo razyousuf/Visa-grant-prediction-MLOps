0349dec0bf94337fc2cca27e097110724d108537
Raz <149075520+razyousuf@users.noreply.github.com>
2025-05-21 15:21:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from uvicorn import run as app_run
 
 from typing import Optional
 
-# from us_visa.constants import APP_HOST, APP_PORT
+from us_visa.constants import APP_HOST, APP_PORT
 from us_visa.pipline.prediction_pipeline import USvisaData, USvisaClassifier
 from us_visa.pipline.training_pipeline import TrainPipeline
 
